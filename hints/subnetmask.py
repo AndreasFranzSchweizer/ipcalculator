3cdbc8469e897db49548e7c9d90106b00180e497
AndreasFranzSchweizer <90528060+AndreasFranzSchweizer@users.noreply.github.com>
2023-05-22 19:12:27 +0200

    Update subnetmask.py
    Changed shift operator to power with base 2

--- hints/subnetmask.py
+++ hints/subnetmask.py
@@ -2,7 +2,7 @@ maskInput = 24
 #defin a number with mask input count of ones
 mask = 2**maskInput-1
 #shift ones to complete 32 bit 
-mask = mask << (32 - maskInput)
+mask = mask * 2 **(32 - maskInput)
 
 #output the value
 print(mask)
