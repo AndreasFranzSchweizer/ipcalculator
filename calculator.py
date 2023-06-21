c924c38c83350f17a9af12bef89e3840cf08e698
Schweizer Andreas <schweizer@wara.de>
2023-06-21 12:58:35 +0200

    restructured code

--- calculator.py
+++ calculator.py
@@ -1,40 +1,55 @@
 #function prints a 32bit integer in dotted decimal notation (DDN)
-def printDDN(address):
+def get_ddn_string(address):
     bytes = [0,0,0,0]
     bytes[0] = address >> 24 & 0xff
     bytes[1] = address >> 16 & 0xff
     bytes[2] = address >> 8 & 0xff
     bytes[3] = address & 0xff
-    print(bytes[0],bytes[1],bytes[2],bytes[3],sep='.')
+    return ".".join(str(item) for item in bytes)
     
-#get user input 
-ipInput = input("ip/mask?") or "192.168.0.1/24"
 
-#split address and mask
-ipInputParts = ipInput.split("/")
-maskInput = ipInputParts[1]
+def get_user_input():
+    #get user input 
+    userInput = input("ip/mask?") or "192.168.0.1/24"
+    #split address and mask
+    userInputParts = userInput.split("/")
+    ipInput = userInputParts[0]
+    maskInput = int(userInputParts[1])
 
-#split octets
-octets = ipInputParts[0].split(".")
+    return ipInput, maskInput
 
-#check ddn length
-if len(octets) != 4:
-    exit()
+def get_int_from_ip(ddn_string):
+    #split octets
+    octets = ddn_string.split(".")
+
+    #check ddn length
+    if len(octets) != 4:
+        exit()
+
+    #convert ddn to int    
+    ip = int(octets[0]) << 24
+    ip += int(octets[1]) << 16
+    ip += int(octets[2]) << 8
+    ip += int(octets[3])
+
+    return ip
     
-#check valid mask
-if int(maskInput) > 32 or int(maskInput) < 1:
-    exit()
-    
-#convert ddn to int    
-ip = int(octets[0]) << 24
-ip += int(octets[1]) << 16
-ip += int(octets[2]) << 8
-ip += int(octets[3])
 
-#defin a number with mask input count of ones
-mask = 2**int(maskInput)-1
-#shift ones to complete 32 bit 
-mask = mask << (32 - int(maskInput))
+def generate_subnet_mask(size):
+    #check valid mask
+    if size > 32 or size < 1:
+        exit()
+    #defin a number with mask input count of ones
+    mask = 2**int(size)-1
+    #shift ones to complete 32 bit 
+    mask = mask << (32 - size)
+
+    return mask
+
+
+ip, subnet_size = get_user_input()
+ip = get_int_from_ip(ip)
+mask = generate_subnet_mask(subnet_size)
 
 #the network address is generated via bitwise and of the ip and the mask
 network = ip & mask
@@ -42,16 +57,10 @@ network = ip & mask
 broadcast = ip | ~mask
 
 #first get the decimal representation of the ip address
-print(ip)
-#print the ip in ddn format
-printDDN(ip)
-#print the subnet mask in ddn format
-printDDN(mask)
-#print the network address in ddn format
-printDDN(network)
-#print the broadcast address in ddn format
-printDDN(broadcast)
-#print the first host ip address
-printDDN(network+1)
-#print the last host ip address
-printDDN(broadcast-1)
+print(f"IP Address decimal representation: {ip}")
+print(f"IP DDN format: {get_ddn_string(ip)}")
+print(f"Subnet mask DDN format: {get_ddn_string(mask)}")
+print(f"Network address DDN format: {get_ddn_string(network)}")
+print(f"Broadcast address DDN format: {get_ddn_string(broadcast)}")
+print(f"First host address DDN format: {get_ddn_string(network+1)}")
+print(f"Last host address DDN format: {get_ddn_string(broadcast-1)}")
